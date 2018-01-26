e38cb9e72d5eb8a709fb2639b42d3d1f6b48eb4f
Chayats <chayat@hotmail.com>
2018-01-26 21:19:29 +0000

    VR functionality room update
    Added torch.
    Added hammer tool and breakable functions
    Scaled VR space to fit wall assets.

---
+++ Pirate-RadioVR/Assets/basher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basher : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("tap");
+        Debug.Log(collision.relativeVelocity.magnitude);
+        if (collision.relativeVelocity.magnitude > 2)
+            Debug.Log("bang");
+    }
+}
---
+++ Pirate-RadioVR/Assets/breakable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakable : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.relativeVelocity.magnitude > 2)
+            Destroy(gameObject);
+    }
+}
