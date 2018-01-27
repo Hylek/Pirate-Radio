8e30afd42f6837e930f65e73659e7497d078e4e5
Chayats <chayat@hotmail.com>
2018-01-27 22:04:13 +0000

    Update level 1
    added floating things, a better screen and made all the handles work

---
+++ Pirate-RadioVR/Assets/zeroGRandomStartMotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class zeroGRandomStartMotion : MonoBehaviour {
+
+    Rigidbody RB;
+
+	// Use this for initialization
+	void Start () {
+
+        Vector3 fudge = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
+
+        RB = GetComponent<Rigidbody>();
+
+
+
+        RB.AddForce(fudge);
+        RB.AddTorque(fudge);
+	}
+	
+
+}
