0bfe5a2c049ba03fbb6a40486f3840b6564b0e9f
Chayats <chayat@hotmail.com>
2018-01-27 12:30:21 +0000

    VR Update
    Made the techybox break-into-able

--- Pirate-RadioVR/Assets/breakable.cs
+++ Pirate-RadioVR/Assets/breakable.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class breakable : MonoBehaviour
 {
+    public GameObject[] unlockables;
 
     void OnCollisionEnter(Collision collision)
     {
 
         if (collision.relativeVelocity.magnitude > 2)
+        {
+
+
+            if (unlockables != null)
+            {
+
+                foreach (GameObject item in unlockables)
+                {
+                    Rigidbody rb = item.GetComponent<Rigidbody>();
+                    rb.isKinematic = false;
+
+
+                }
+            }
+
             Destroy(gameObject);
+
+        }
     }
 }
