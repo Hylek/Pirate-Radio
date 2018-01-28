52ed30cde3f56f434464c5db67dce13ef0cf147e
Daniel Cumbor <dcdolphy@gmail.com>
2018-01-28 05:57:41 +0000

    Added switchboard script.

---
+++ Pirate-RadioVR/Assets/SwitchBoardController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBoardController : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip godSake;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void PlayGodSake()
+    {
+        audioSource.clip = godSake;
+        audioSource.Play();
+    }
+
+}
