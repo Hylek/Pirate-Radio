80742d40b13f1f85b050bc4361c11f60f3a2bba8
Daniel Cumbor <dcdolphy@gmail.com>
2018-01-28 00:17:31 +0000

    Updated lighting in area1light

---
+++ Pirate-RadioVR/Assets/Scripts/NewDoorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewDoorScript : MonoBehaviour {
+
+    public Vector3 targetHeight;
+    private Vector3 startPoint;
+    public bool player;
+
+	// Use this for initialization
+	void Start () {
+        startPoint = transform.position;
+        //player = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(startPoint.y < targetHeight.y && player)
+        {
+            transform.Translate(Vector3.up * 0.03f);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.gameObject.name == "Player")
+        {
+            player = true;
+        }
+    }
+}
