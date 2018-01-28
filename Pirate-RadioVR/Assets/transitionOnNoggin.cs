e8cb5486a5b0597d857b44e0ea7763f2fcaca392
Chayats <chayat@hotmail.com>
2018-01-28 10:56:40 +0000

    Update Level 1
    Added transition to level 2
    Added mesh colliders to the walls

---
+++ Pirate-RadioVR/Assets/transitionOnNoggin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transitionOnNoggin : MonoBehaviour {
+
+    public MonoBehaviour transitioningscript;
+
+
+
+
+        void OnCollisionEnter(Collision collision)
+    {
+
+        transitioningscript.enabled = true;
+
+
+
+    }
+
+    }
+
+
